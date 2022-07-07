01eb702806ee72117ccd2275d2f7a90eee315036
Asterios Vogiatzis <a.vogiatzis@epsilon-singularlogic.eu>
2022-07-07 11:01:20 +0300

    Book-recipe first view

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This simply a Test',
+    'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2018%2F04%2F19%2Fmrtrending0475-2000.jpg') 
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
